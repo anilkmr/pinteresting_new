21fd40182985d1a659cafe183a80ea520470ce9d
Anil Kumar <akumar05t@gmail.com>
2014-01-08 17:21:27 -0500

    Recommit after paperclip issues

---
+++ README.md
@@ -0,0 +1,6 @@
+# One mOnth Rails
+
+This is the pinteresting sample application for 
+[*One Month Rails*](http://onemonthrails.com)
+
+by Anil Kumar (mailto:akumar05t@gmail.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = '946b108affd318acf1022fa572e0276c624f6312dbf6b825e4d5ef2a6d5cf720aa3dbe0560fbb6438a048a7e7c1d27f1614b28b9fa244f50adad1368a6a95e5d'
